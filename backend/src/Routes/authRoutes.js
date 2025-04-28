6b9f1b2b2e508ee34636972db5a6a7593f44824a
GabrielEsquiavoni <gabrielazevedo.2004@gmail.com>
2025-04-28 18:01:59 -0300

    Registro de Usuário e Hash de Senha adicionados.

--- backend/src/Routes/authRoutes.js
+++ backend/src/Routes/authRoutes.js
@@ -1,9 +1,49 @@
 import express from "express";
+import User from "../Models/User.js";
 
 const router = express.Router();
 
-router.get("/register", async (req, res) => {
-  res.send("Register route");
+router.post("/register", async (req, res) => {
+  try{
+    const {email,username,password} = req.body;
+
+    if(!username || !email || !password){
+      return res.status(400).json({message: "Please fill all fields"});
+    }
+
+    if(password.length < 6){
+      return res.status(400).json({message: "Password must be at least 6 characters"});
+    }
+
+    if(username.length < 3){
+      return res.status(400).json({message: "Username must be at least 3 characters"});
+    }
+
+    // check if user already exists
+
+    const existingEmail = await User.findOne({email});
+    if(existingEmail){
+      return res.status(400).json({message: "Email already exists"});
+    }
+
+    const existingUsername = await User.findOne({username});
+    if(existingUsername){
+      return res.status(400).json({message: "Username already exists"});
+    }
+    
+    // get random avatar
+    const profileImage = `https://api.dicebear.com/9.x/lorelei/svg?seed=${username}`;
+
+    const user = new User({
+      username,
+      email,
+      password,
+      profileImage,
+    })
+  } catch (error) {
+    console.error("Error in register route", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 router.post("/login", async (req, res) => {
